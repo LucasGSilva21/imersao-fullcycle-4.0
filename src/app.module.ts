5089dc11bcc58545ea1d0d843a4905db544e4a97
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2021-09-14 09:13:14 -0300

    chore: add sequelize in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TransactionsModule } from './transactions/transactions.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [TransactionsModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: process.env.DB_CONNECTION as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      models: [],
+      autoLoadModels: true,
+      synchronize: true,
+      sync: {
+        alter: true,
+      },
+    }),
+    TransactionsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
