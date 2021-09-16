d1e1987979650bae4f5d218d79557207671a3a20
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2021-09-16 08:59:10 -0300

    feat: config theme

--- nextjs-front/src/pages/_app.tsx
+++ nextjs-front/src/pages/_app.tsx
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import theme from '../utils/theme';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    jssStyles?.parentElement?.removeChild(jssStyles);
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+       <Component {...pageProps} />
+    </ThemeProvider>
+  ); 
+   
 }
-export default MyApp
+export default MyApp;
