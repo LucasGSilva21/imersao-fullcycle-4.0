9e6a15df63cb2ddf0892a51cc4507b0ce8fcc581
Lucas Gabriel <lucasgsilva2102@gmail.com>
2021-10-04 14:04:05 -0300

    feat: att balance

--- nestjs-api/src/transactions/transactions.service.ts
+++ nestjs-api/src/transactions/transactions.service.ts
@@ -1,21 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
-import { Transaction } from './entities/transaction.entity';
+import { Transaction, TransactionType } from './entities/transaction.entity';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { TenantService } from 'src/tenant/tenant/tenant.service';
-
+import { Sequelize } from 'sequelize-typescript';
+import { Account } from './../accounts/entities/account.entity';
 @Injectable()
 export class TransactionsService {
   constructor(
     @InjectModel(Transaction) private transactionModel: typeof Transaction,
+    @InjectModel(Account) private accountModel: typeof Account,
     private tenantService: TenantService,
+    private sequelize: Sequelize,
   ) {}
 
-  create(createTransactionDto: CreateTransactionDto) {
-    return this.transactionModel.create({
-      ...createTransactionDto,
-      account_id: this.tenantService.tenant.id,
-    });
+  async create(createTransactionDto: CreateTransactionDto) {
+    const atomic = await this.sequelize.transaction();
+
+    try {
+      const transaction = await this.transactionModel.create({
+        ...createTransactionDto,
+        account_id: this.tenantService.tenant.id,
+      });
+      const account = await this.accountModel.findByPk(transaction.account_id, {
+        lock: atomic.LOCK.UPDATE,
+        transaction: atomic,
+      });
+      const amount =
+        createTransactionDto.type === TransactionType.DEBIT
+          ? -transaction.amount
+          : transaction.amount;
+      await account.update(
+        { balance: account.balance + amount },
+        { transaction: atomic },
+      );
+      await atomic.commit();
+      return transaction;
+    } catch (e) {
+      await atomic.rollback();
+      throw e;
+    }
   }
 
   findAll() {
